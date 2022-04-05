8d3668af4871e4c66b16b141abb58aa7b0fef575
kaushikkumarbora <kaushikkumarbora@gmail.com>
2022-04-05 11:56:18 +0530

    Moved Creds to .env
    Signed-off-by: kaushikkumarbora <kaushikkumarbora@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GetMentoring
+
+Create a `.env` file in `server\database\` using `example.env` with password and user.
+
 Looks like I chose a very bad ORM. No Error handling for ConnectionErrors :(
--- server/database/config.js
+++ server/database/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     HOST: "database",
-    USER: "postgres",
+    USER: process.env.POSTGRES_USER,
     PORT: 5432,
-    PASSWORD: "kaushik",
-    DB: "Mentorship",
+    PASSWORD: process.env.POSTGRES_PASSWORD,
+    DB: process.env.POSTGRES_DB,
     dialect: "postgres",
     pool: {
         max: 5,
